b914b60598dcd182274622ba08c33f970487c45a
Manisha Gadilkar <manishagadilkar93@gmail.com>
2024-07-03 19:54:53 +0530

    Added test for add method when given an empty string

---
+++ string_calculator_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require './string_calculator'
+
+RSpec.describe StringCalculator do
+  subject(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(calculator.add('')).to eq(0)
+      end
+    end
+  end
+end
