72dc26815a9b8622bb5de44dbda20d657b5d69c9
Manisha Gadilkar <manishagadilkar93@gmail.com>
2024-07-03 20:09:55 +0530

    Added add method for Return 0 for empty strings

---
+++ string_calculator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class StringCalculator
+  def add(numbers)
+    # Return 0 for empty strings
+    return 0 if numbers.empty?
+  end
+end
+  
+# Example usage:
+# calculator = StringCalculator.new
+# puts calculator.add('')          # Should output 0
